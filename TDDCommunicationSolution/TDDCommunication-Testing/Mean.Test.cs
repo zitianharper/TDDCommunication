d11c7632ae85206042fbabb8ae29eab9b15a7aee
Marcus Paterson <marcus_paterson@outlook.com>
2026-03-24 15:54:50 +0000

    Completed task 2, added Mean and functionality

---
+++ TDDCommunicationSolution/TDDCommunication-Testing/Mean.Test.cs
@@ -0,0 +1,22 @@
+﻿using Shouldly;
+using TDDCommunication;
+
+namespace TDDCommunication_Testing;
+
+public class MeanTest
+{
+  
+    [Test]
+    public void ShouldReturnMeanFromArray()
+    {
+        //arrange
+        Mean mean = new Mean();
+        int[] numbers = [1, 2, 3, 4, 5];
+
+        //act
+        double output = mean.MeanMethod(numbers);
+
+        //assert
+        output.ShouldBe(3);
+    }
+}
---
+++ TDDCommunicationSolution/TDDCommunication/Mean.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDCommunication
+{
+    public class Mean
+    {
+        public double MeanMethod(int[] numbers)
+        {
+            double output = numbers.Average();
+            return output;
+
+        }
+
+    }
+}
