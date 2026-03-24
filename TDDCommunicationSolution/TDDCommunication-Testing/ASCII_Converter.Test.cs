ee875c5fea85b6ebaf37aed369d12b5f9dd58c50
Marcus Paterson <marcus_paterson@outlook.com>
2026-03-24 16:59:58 +0000

    ASCII Converter test

---
+++ TDDCommunicationSolution/TDDCommunication-Testing/ASCII_Converter.Test.cs
@@ -0,0 +1,36 @@
+﻿using Shouldly;
+using TDDCommunication;
+
+namespace TDDCommunication_Testing;
+
+public class ASCII_ConverterTest
+{
+  
+    [Test]
+    public void ShouldReturn97_WhenGivenA()
+    {
+        ////arrange
+        ASCII_Converter aSCII_Converter = new ASCII_Converter();
+        string[] letters = ["a"] ;
+
+        ////act
+        int output = aSCII_Converter.ASCII_ConverterMethod(letters);
+
+        ////assert
+        output.ShouldBe(97);
+    }
+
+    [Test]
+    public void ShouldSplitStringArray()
+    {
+        ////arrange
+        ASCII_Converter aSCII_Converter = new ASCII_Converter();
+        string input = "no";
+
+        ////act
+        string[] letters = aSCII_Converter.Splitter(input);
+
+        ////assert
+        letters.ShouldBe([ "n", "o"]);
+    }
+}
---
+++ TDDCommunicationSolution/TDDCommunication/ASCII_Converter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDCommunication
+{
+    public class ASCII_Converter
+    {
+        string input = "no";
+        public string[] Splitter(string input)
+        {
+            string[] splitChars = input.Split("");
+            string [] letters = splitChars.ToArray();
+            return letters;
+        }
+
+
+        public int ASCII_ConverterMethod(string[] letters)
+        {
+            int output = (int)'a';
+
+            return output;
+
+        }
+
+    }
+}
